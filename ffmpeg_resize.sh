02b5eaa8e58a4ef26aa570250299baebf2a912f5
Pablo Wakonigg <pablowako@gmail.com>
2022-02-07 18:03:20 +0100

    Update ffmpeg_resize.sh

--- ffmpeg_resize.sh
+++ ffmpeg_resize.sh
@@ -1,4 +1,4 @@
 echo Input desired resolution
 read res
 mkdir $res
-for i in *.jpg; do ffmpeg -i "$i" -vf scale=$res:-1 "/home/pablo/Documents/GitHub/plazadeitalia/assets/$res/${i%.*}_$res.jpg"; done
+Foreach ($file in get-content /){ffmpeg -i "$i" -vf scale=$res:-1 "/$res/${i%.*}_$res.jpg"}
